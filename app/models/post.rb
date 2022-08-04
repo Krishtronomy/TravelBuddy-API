b635f614c7203773be419a0d37f420b668ccfcf5
Krish <krishal@myxplor.com>
2022-08-05 02:46:05 +1000

    Added rspec testing accross all actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    validates_presence_of :description
 
 def transform_post
     return {
---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :post do
+      description { "MyString" }
+    end
+  end
+  
